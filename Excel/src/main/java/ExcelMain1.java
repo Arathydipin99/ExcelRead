738e218c208b9fef007094b16e72066fcada5f18
Arathy <Arathy@DSNPVT>
2023-07-05 00:41:16 +0530

    Excel project committed

---
+++ Excel/src/main/java/ExcelMain1.java
@@ -0,0 +1,13 @@
+
+import java.io.IOException;
+public class ExcelMain1 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+String d=ExcelCode1.getStringData(1, 1);
+System.out.println(d);
+String e=ExcelCode1.getIntegerData(1, 0);
+System.out.println(e);
+	}
+
+}
